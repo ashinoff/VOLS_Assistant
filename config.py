5dfc4d4485dd797b1d90dbd85089a6b408b57a6f
ashinoff <ashinoff@gmail.com>
2025-07-08 17:46:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,21 +4,22 @@ TOKEN = os.getenv("TOKEN", "8066747417:AAG3nzy1wuK4uogGYwUrWZtZyUxKdx-7ufQ")
 SELF_URL = os.getenv("SELF_URL", "https://vols-assistant.onrender.com")
 PORT = int(os.getenv("PORT", 5000))
 
+# Динамическая сборка словарей из переменных окружения
 BRANCH_URLS = {
     "Россети ЮГ": {
-        "Тимашевские ЭС": os.getenv("BRANCH_URLS_UG", ""),
+        "Тимашевские ЭС": os.getenv("TIMASHEV_ES_URL_UG", ""),
     },
     "Россети Кубань": {
-        "Тимашевские ЭС": os.getenv("BRANCH_URLS_RK", ""),
+        "Тимашевские ЭС": os.getenv("TIMASHEV_ES_URL_RK", ""),
     },
 }
 
 NOTIFY_URLS = {
     "Россети ЮГ": {
-        "Тимашевские ЭС": os.getenv("NOTIFY_URLS_UG", ""),
+        "Тимашевские ЭС": os.getenv("TIMASHEV_ES_URL_UG_SP", ""),
     },
     "Россети Кубань": {
-        "Тимашевские ЭС": os.getenv("NOTIFY_URLS_RK", ""),
+        "Тимашевские ЭС": os.getenv("TIMASHEV_ES_URL_RK_SP", ""),
     },
 }
 

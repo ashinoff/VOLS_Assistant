f062fbdf0d4fb0a554803d80aa269f0dcb38292b
ashinoff <ashinoff@gmail.com>
2025-07-11 01:07:35 +0300

    Update autoping.py

--- utils/autoping.py
+++ utils/autoping.py
@@ -1,1 +1,19 @@
+import threading
+import requests
+import time
+import os
+
+def autoping():
+    url = os.getenv("PING_URL", "")
+    if not url:
+        return
+    while True:
+        try:
+            requests.get(url + "/ping", timeout=10)
+        except Exception:
+            pass
+        time.sleep(600)  # Каждые 10 минут
+
+def start_ping():
+    threading.Thread(target=autoping, daemon=True).start()
 
